685404b0eab1ba6170c36eb23afe0f5ae6684b27
arunp.info <arunp.info@gmail.com>
2023-06-25 16:12:51 +0530

    Create azure-sql-var.tf

---
+++ azure-sql-var.tf
@@ -0,0 +1,28 @@
+variable "resource_group_location" {
+  type        = string
+  default     = "eastus-2"
+}
+
+variable "resource_group_name" {
+  type        = string
+  default     = "az-eastus-rg-applicationname"
+}
+
+variable "sql_db_name" {
+  type        = string
+  description = "The name of the SQL Database."
+  default     = "testdatabase"
+}
+
+variable "admin_username" {
+  type        = string
+  description = "The administrator username of the SQL logical server."
+  default     = "azureadmin"
+}
+
+variable "admin_password" {
+  type        = string
+  description = "The administrator password of the SQL logical server."
+  sensitive   = true
+  default     = null
+}
